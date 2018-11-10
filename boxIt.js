0e916fc8000071ee17138dbe19b98db0aad605d3
Ming Chai <ming.chai4@gmail.com>
2018-11-09 18:07:17 -0800

    Create first postion of assignment

---
+++ boxIt.js
@@ -0,0 +1,21 @@
+let userInput = process.argv[2];
+
+function drawLine(num){
+    let arr = [];
+    for(let i = 0; i <userInput.length; i++){
+        arr.push('-');
+    }
+    console.log(arr.join(''));
+}
+
+console.log(drawLine());
+
+function drawLine(num){
+    let arr = [];
+    for(let i = 0; i <userInput.length; i++){
+        arr.push('-');
+    }
+    console.log(arr.join(''));
+}
+
+console.log(drawLine());
