b9178dd190c279cde48848ce682e0cff2f4a94ae
Ming Chai <ming.chai4@gmail.com>
2018-11-09 21:17:27 -0800

    Add code for drawTopLine

--- boxIt.js
+++ boxIt.js
@@ -2,20 +2,20 @@ let userInput = process.argv[2];
 
 function drawLine(num){
     let arr = [];
-    for(let i = 0; i <userInput.length; i++){
+    for(let i = 0; i < userInput.length; i++){
         arr.push('-');
     }
     console.log(arr.join(''));
 }
 
-console.log(drawLine());
+console.log(drawLine(4));
 
-function drawLine(num){
-    let arr = [];
-    for(let i = 0; i <userInput.length; i++){
-        arr.push('-');
+function drawTopLine(num){
+    let arr1 = [];
+    for(let i = 1; i < userInput.length; i++){
+        arr1.push('-');
     }
-    console.log(arr.join(''));
+    console.log('\u250f' + arr1.join('')+ '\u2513');
 }
 
-console.log(drawLine());
+console.log(drawTopLine());
