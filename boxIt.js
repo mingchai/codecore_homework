29db08f52db74d4bfa4747b4e3dfdf2c4374a1b6
Ming Chai <ming.chai4@gmail.com>
2018-11-14 19:09:17 -0800

    Add function for top, middle, and bottom border

--- boxIt.js
+++ boxIt.js
@@ -1,21 +1,44 @@
-let userInput = process.argv[2];
-
+let userInput = parseInt(process.argv[2]);
+// ------ GENERAL LINE ------
 function drawLine(num){
     let arr = [];
-    for(let i = 0; i < userInput.length; i++){
-        arr.push('-');
+    for(let i = 0; i < num; i++){
+        arr.push('\u2501');
     }
     console.log(arr.join(''));
 }
 
-console.log(drawLine(4));
+console.log(drawLine(userInput));
 
+// ------ TOP LINE ------
 function drawTopLine(num){
     let arr1 = [];
-    for(let i = 1; i < userInput.length; i++){
-        arr1.push('-');
+    for(let i = 1; i < num; i++){
+        arr1.push('\u2501');
     }
     console.log('\u250f' + arr1.join('')+ '\u2513');
 }
 
-console.log(drawTopLine());
+console.log(drawTopLine(userInput));
+
+// ------ MIDDLE LINE ------
+function drawMiddleLine(num){
+    let arr1 = [];
+    for(let i = 1; i < num; i++){
+        arr1.push('\u2501');
+    }
+    console.log('\u2523' + arr1.join('')+ '\u252b');
+}
+
+console.log(drawMiddleLine(userInput));
+
+// ------ BOTTOM LINE ------
+function drawBottomLine(num){
+    let arr1 = [];
+    for(let i = 1; i < num; i++){
+        arr1.push('\u2501');
+    }
+    console.log('\u2517' + arr1.join('')+ '\u251b');
+}
+
+console.log(drawBottomLine(userInput));
