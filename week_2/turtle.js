aab350fa4beae55244eaa586da0f3bb5e7edf09f
Ming Chai <ming.chai4@gmail.com>
2018-11-20 08:54:59 -0800

    make basic structure for turtle.js and deleted unrelated file

---
+++ week_2/turtle.js
@@ -0,0 +1,37 @@
+const Turtle = {
+    count: 0,
+    forward(num){
+        this.count += num;
+    },
+    right(){
+        this.count += 0;
+    },
+    left(){
+        this.count += 0;
+    },
+    allPoints(){
+        let log = [];
+
+    }
+}
+
+class Turtle{
+    constructor (x,y){
+        this.x = x;
+        this.y = y;
+    }
+
+    forward(){
+
+    }
+
+    right(){
+
+    }
+    left(){
+
+    }
+    allPoints(){
+
+    }
+}
