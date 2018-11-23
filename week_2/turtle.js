3a58451536dbe626fbfff9637fbb7fa43af3d879
Ming Chai <ming.chai4@gmail.com>
2018-11-22 19:00:17 -0800

    Add left, right, and allPoints and simple test cases

--- week_2/turtle.js
+++ week_2/turtle.js
@@ -10,18 +10,18 @@ class Turtle{
     forward(num){
 	
         for(let stepCount = 0; stepCount < num+1; stepCount++){
-            if(this.turtleCompass == 90){
-                this.turtleTravelLog.push([this.x, this.y +stepCount]);
+            if(this.turtleCompass == 90 || this.turtleCompass == -270){
+                this.turtleTravelLog.push([this.x, this.y += 1]);
             } else if(this.turtleCompass == 0){
-                this.turtleTravelLog.push([this.x + stepCount, this.y]);
+                this.turtleTravelLog.push([this.x += 1, this.y]);
             } else if(this.turtleCompass == 180){
-                this.turtleTravelLog.push([this.x + stepCount, this.y]);
-            } else if(this.turtleCompass == 270){
-                this.turtleTravelLog.push([this.x, this.y - stepCount]);
-            } else if(this.turtleCompass == 360){
+                this.turtleTravelLog.push([this.x += 1, this.y]);
+            } else if(this.turtleCompass == 270 ||this.turtleCompass == -90 ){
+                this.turtleTravelLog.push([this.x, this.y -= 1]);
+            } else if(this.turtleCompass == 360 ||this.turtleCompass == -360){
                 this.turtleCompass = 0;
-            } else if(this.turtleCompass < 0){
-                this.turtleCompass * -1;
+            } else if(this.turtleCompass == -180){
+                this.turtleTravelLog.push([this.x -= 1, this.y]);
             }
         }
             
@@ -30,16 +30,21 @@ class Turtle{
     }
     
     right(){
-
+        this.turtleCompass += 90;
+        return this;
     }
 
     left(){
-
+        this.turtleCompass -= 90;
+        return this;
     }
 
     allPoints(){
-
+        return this.turtleTravelLog;
     }
 };
 let turtle = new Turtle(0,0)
-console.log(turtle.forward(4));
+// console.log(turtle.forward(4));
+// console.log(turtle.forward(4).right().forward(4));
+// console.log(turtle.forward(4).left().forward(4));
+console.log(turtle.forward(4).left().forward(4).allPoints());
