428031311ccfc64f23dece9c316e4148c80db153
Ming Chai <ming.chai4@gmail.com>
2018-11-29 17:44:27 -0800

    added welcome prompt

--- week_3/toDoCLI.js
+++ week_3/toDoCLI.js
@@ -1,11 +1,12 @@
 // ----- LOAD REQUIRED MODULES -----
-const readline = require("readine");
+const readline = require("readline");
 const fs = require("fs");
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
+    prompt: "Welcome to Todo CLI! \n -------------------- \n (v) 'View' • (n) 'New' • (cX) 'Complete' • (dX) 'Delete' • (q) 'Quit'"
 });
 
 // ----- HOMEPAGE VIEW -----
-rl.question("Welcome to Todo CLI! \n -------------------- \n (v) View • (n) New • (cX) Complete • (dX) Delete • (q) Quit", answer);
+rl.prompt();
