6edc01cf628c1dc314b687b8c21eb4ec72d10f52
Ming Chai <ming.chai4@gmail.com>
2018-11-29 17:58:34 -0800

    sample functionality added

--- week_3/toDoCLI.js
+++ week_3/toDoCLI.js
@@ -5,8 +5,30 @@ const fs = require("fs");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-    prompt: "Welcome to Todo CLI! \n -------------------- \n (v) 'View' • (n) 'New' • (cX) 'Complete' • (dX) 'Delete' • (q) 'Quit'"
+    prompt: "Welcome to Todo CLI! \n -------------------- \n (v) 'View' • (n) 'New' • (cX) 'Complete' • (dX) 'Delete' • (q) 'Quit' \n"
 });
 
 // ----- HOMEPAGE VIEW -----
 rl.prompt();
+
+rl.on('line', (line) => {
+    switch (line.trim()) {
+      case 'v':
+        console.log('View Page!');
+        break;
+      case 'n':
+        console.log('New Page!');
+        break;
+      case 'q':
+        console.log('See you later!');
+        rl.close();
+        break;
+      default:
+        console.log(`I didn't quite catch that'`);
+        break;
+    }
+    // rl.prompt();
+  }).on('close', () => {
+    console.log('Have a great day!');
+    process.exit(0);
+  });
