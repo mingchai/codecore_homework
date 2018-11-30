f851b183365c0dbde9e14288ef78500b27cd04c9
Ming Chai <ming.chai4@gmail.com>
2018-11-29 20:14:26 -0800

    HW3 - created add item functionality

--- week_3/toDoCLI.js
+++ week_3/toDoCLI.js
@@ -5,19 +5,30 @@ const fs = require("fs");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-    prompt: "Welcome to Todo CLI! \n -------------------- \n (v) 'View' • (n) 'New' • (cX) 'Complete' • (dX) 'Delete' • (q) 'Quit' \n"
+    prompt: "Welcome to Todo CLI! \n-------------------- \n (v) 'View' • (n) 'New' • (cX) 'Complete' • (dX) 'Delete' • (q) 'Quit' \n"
 });
 
 // ----- HOMEPAGE VIEW -----
 rl.prompt();
+const options = "\n(v) 'View' • (n) 'New' • (cX) 'Complete' • (dX) 'Delete' • (q) 'Quit' \n"
 
 rl.on('line', (line) => {
     switch (line.trim()) {
       case 'v':
-        console.log('View Page!');
+        // console.log('Welcome to Your View Page!');
+        fs.readFile("./toDoList.txt", "utf8", (error, data) =>{
+            const toDoList = data
+            if (error){
+                return console.error(error);
+            } else{
+                console.log("Welcome to your list!")
+                console.log(toDoList);
+                console.log(options);
+            }
+        })
         break;
       case 'n':
-        console.log('New Page!');
+        rl.question("What would you like to add? \n", addItem)
         break;
       case 'q':
         console.log('See you later!');
@@ -32,3 +43,14 @@ rl.on('line', (line) => {
     console.log('Have a great day!');
     process.exit(0);
   });
+
+//   ----- FUNCTIONS -----
+function addItem(answer){
+    let userInput = `\n• ${answer}`;
+    fs.appendFile("./toDoList.txt", userInput, (err)=>{
+        if(err){
+            console.error(err);
+        }
+    })
+    console.log(`You added: ${userInput}!`);
+}
