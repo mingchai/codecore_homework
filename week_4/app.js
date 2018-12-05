ba85b2a4b4107d9cc7ea216c9aa4623b74a96ea4
Ming Chai <ming.chai4@gmail.com>
2018-12-05 09:07:17 -0800

    shuffle and display of data available. Reworking of code needed to display on correct page

--- week_4/app.js
+++ week_4/app.js
@@ -1,11 +1,51 @@
 const express = require("express");
 const logger = require("morgan");
 const app = express();
+const knex = require("./db/client");
+const shuffle = require("shuffle-array");
 
 app.use(logger("dev"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: false}));
 
+app.get("/", (req,res)=>{
+    res.render("homePage");
+});
+
+app.get("/new_cohort", (req, res)=>{
+    res.render("newCohort");
+});
+
+app.post("/new_cohort", (req, res) =>{
+    const logoUrl = req.body.logoUrl;
+    const name = req.body.name;
+    const members = req.body.members;
+
+    const newCohort = {
+        logoUrl,
+        name,
+        members
+    };
+
+    knex("teams").insert(newCohort).returning("*").then(()=>{
+        res.redirect("/index")
+    });
+});
+
+app.get("/index", (req, res)=>{
+
+    knex.select('*').from('teams').then(cohortData =>{
+        revCohortData = cohortData.map(cohort =>{
+            let revCohort = cohort.members.split(",");
+            
+            let revCohortData = shuffle(revCohort);
+            console.log(revCohortData)
+            res.render("index", {cohortData, revCohortData});
+        });
+        // res.render("index", {cohortData, revCohortData});
+    });
+
+})
 
 // SERVER SETUP
 const PORT = 5670;
