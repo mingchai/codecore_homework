f5cf3830cea7c2f33cd5804a67ac4b4bb455ee10
Ming Chai <ming.chai4@gmail.com>
2019-01-12 20:31:52 -0800

    renamed ids on underscores generated for letter placement

--- HW_8/index.js
+++ HW_8/index.js
@@ -1,33 +1,64 @@
-for(let i = 0; i < 26; i++){
+function letterGenerator(){
+    for(let i = 0; i < 26; i++){
     
-    let divs = document.createElement("div");
-    let letter = (i+10).toString(36).toUpperCase();
-    divs.innerText = letter;
-    // divs.outerHTML("div");
-    // divs.innerHTML = "<div>";
-    // divs.classList.add("container");
-    // divs.style.border = "thin solid black";
-    // divs.style.borderRadius = "100%";
-    document.querySelector("div").append(divs);
+        let divs = document.createElement("div");
+        let letter = (i+10).toString(36).toUpperCase();
+    
+        divs.innerText = letter;
+        document.querySelector("div").append(divs);
+    };
+};
+
+letterGenerator();
+
+let secretWord = "test".toUpperCase();
+let span = document.createElement("span");
+document.querySelector("#letterInput").append(span);
+
+for(let i = 0; i < secretWord.length; i++){
+    const dashes = document.createElement("div");
+    dashes.innerText = "_ ";
+    dashes.classList.add("dashed");
+    dashes.setAttribute("id", i);
+    document.querySelector("span").append(dashes);
 }
 
 function select(){
-    document.querySelectorAll("div > div").forEach( div =>{
+    document.querySelectorAll("#letterBoxes > div").forEach( div =>{
         div.addEventListener("click", event => {
             let selectedLetter = event.currentTarget;
             
             
             if(selectedLetter.className == "selected"){
                 selectedLetter.classList.remove("selected");
-                selectedLetter.style.backgroundColor = "red";
             } else {
                 selectedLetter.classList.add("selected");
-                selectedLetter.style.backgroundColor = "";
+                selectedLetter.classList.remove("style");
             }
-            
+
+            let selection = Array.from(document.querySelectorAll(".selected"));
+            for(let obj of selection){
+                console.log(obj.innerText);
+                console.log(event.target.innerText);
+                for(let keys of secretWord){
+                    console.log(keys);
+                    let {keys} = secretWord;
+                }
+                if(event.target.innerText == "t"){
+                    let x = document.querySelector("#dashed")
+                        dashes.innerText = secretWord[0];
+                    }
+            }
+            // console.log(selection);
+            // let letter = event.target.innerText;
+                // if(selection[0] == secretWord[0]){
+                //     dashes.innerText = secretWord[0];
+                // }
         })
     });
-}
+};
 
 select();
+
+
 
