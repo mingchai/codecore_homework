c09a60beab84405f2bdda9ea11f55be70abfa12d
Ming Chai <ming.chai4@gmail.com>
2019-01-16 00:44:56 -0800

    set image generation on incorrect answers

--- HW_8/index.js
+++ HW_8/index.js
@@ -5,20 +5,21 @@ function letterGenerator(){
         let letter = (i+10).toString(36).toUpperCase();
     
         divs.innerText = letter;
+        divs.style.border = "solid thin black"
         document.querySelector("div").append(divs);
     };
 };
 
 letterGenerator();
 
-const secretWord = "test".toUpperCase();
+const secretWord = "testing".toUpperCase();
 
 function blankGenerator(){
     for(let i = 0; i < secretWord.length; i++){
         const dashes = document.createElement("div");
         dashes.innerText = ' ';
         dashes.classList.add(`dashed-${i}`);
-        // dashes.setAttribute("id", `ltr`);
+        
         document.querySelector("#letterInput").append(dashes);
     };
 }
@@ -26,31 +27,41 @@ function blankGenerator(){
 blankGenerator();
 
 function select(){
+
+    const img = "./assets/gallows.jpg"
+    const img1 = "./assets/head.jpg";
+    const img2 = "./assets/torso.jpg";
+    const img3 = "./assets/left-leg.jpg";
+    const img4 = "./assets/right-leg.jpg";
+    const img5 = "./assets/left-arm.jpg";
+    const img6 = "./assets/right-arm.jpg";
+    const images = [img,img1, img2, img3, img4, img5, img6];
+
     document.querySelectorAll("#letterBoxes > div").forEach( div =>{
         div.addEventListener("click", event => {
             let selection = event.currentTarget;
             let selectedLetter = event.currentTarget.innerText;
             
             // change color of selected letter
-            if(selection.className == "selected"){
-                selection.classList.remove("selected");
-            } else {
                 selection.classList.add("selected");
-                selection.classList.remove("style");
-            }
+                selection.style.border = "solid thin red"
 
-            // Compare target to secret word
+
+            // Compare target to secret word - find correct answers only
             for(let i = 0; i < secretWord.length; i++){
                 if(selectedLetter == secretWord[i]){
                     document.querySelector(`.dashed-${i}`).innerText = secretWord[i];
-                }
-            }
+                    selection.setAttribute("class", "correct")
+                } 
+            };
+
+            // Set gallows image based on number of incorrect guesses
+            let mistakes = document.querySelectorAll(".selected").length;
+			console.log("​select -> mistakes", mistakes)
+            document.querySelector("img").setAttribute("src", `${images[mistakes]}`);
             
-        })
+        });
     });
 };
 
 select();
-
-
-
