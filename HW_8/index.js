71451e14684c4380d2f1e8de26480d531494f7a5
Ming Chai <ming.chai4@gmail.com>
2019-01-15 17:52:11 -0800

    fixed placement and visibility of blank fields

--- HW_8/index.js
+++ HW_8/index.js
@@ -12,23 +12,28 @@ function letterGenerator(){
 letterGenerator();
 
 let secretWord = "test".toUpperCase();
-let span = document.createElement("span");
-document.querySelector("#letterInput").append(span);
 
-for(let i = 0; i < secretWord.length; i++){
-    const dashes = document.createElement("div");
-    dashes.innerText = "_ ";
-    dashes.classList.add("dashed");
-    dashes.setAttribute("id", i);
-    document.querySelector("span").append(dashes);
+function blankDashGen(){
+    // let span = document.createElement("span");
+    // document.querySelector("#letterInput").append(span);
+    
+    for(let i = 0; i < secretWord.length; i++){
+        const dashes = document.createElement("div");
+        dashes.innerText = ' ';
+        dashes.classList.add("dashed");
+        dashes.setAttribute("id", i);
+        document.querySelector("#letterInput").append(dashes);
+    };
 }
+
+blankDashGen();
 
 function select(){
     document.querySelectorAll("#letterBoxes > div").forEach( div =>{
         div.addEventListener("click", event => {
             let selectedLetter = event.currentTarget;
             
-            
+            // change color of selected letter
             if(selectedLetter.className == "selected"){
                 selectedLetter.classList.remove("selected");
             } else {
@@ -40,20 +45,11 @@ function select(){
             for(let obj of selection){
                 console.log(obj.innerText);
                 console.log(event.target.innerText);
-                for(let keys of secretWord){
-                    console.log(keys);
-                    let {keys} = secretWord;
-                }
                 if(event.target.innerText == "t"){
                     let x = document.querySelector("#dashed")
                         dashes.innerText = secretWord[0];
                     }
             }
-            // console.log(selection);
-            // let letter = event.target.innerText;
-                // if(selection[0] == secretWord[0]){
-                //     dashes.innerText = secretWord[0];
-                // }
         })
     });
 };
